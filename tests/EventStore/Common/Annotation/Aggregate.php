bd65c36954f6e725693243bc85b379866b49d746
Daniel Santamaria <santaka87@gmail.com>
2016-10-26 23:55:55 +0200

    PublishDomainEvent annotation added.

--- tests/EventStore/Common/Annotation/Aggregate.php
+++ tests/EventStore/Common/Annotation/Aggregate.php
@@ -3,11 +3,58 @@
 namespace Tests\EventStore\Common\Annotation;
 
 use EventStore\Common\Annotation\EventSourcedAggregate;
+use EventStore\Common\Annotation\PublishDomainEvent;
 
 /**
  * @EventSourcedAggregate()
  */
 class Aggregate
 {
+    /**
+     * @var string
+     */
+    private $id;
 
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @param string $id
+     * @param string $name
+     *
+     * @PublishDomainEvent(event="AggregateAdded")
+     */
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function id()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @PublishDomainEvent(event="AggregateNameChanged")
+     */
+    public function changeName($name)
+    {
+        $this->name = $name;
+    }
 }
---
+++ tests/EventStore/Common/Annotation/PublishDomainEventTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace tests\EventStore\Common\Annotation;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use EventStore\Common\Annotation\PublishDomainEvent;
+
+class PublishDomainEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldReadPublishDomainEventAnnotation()
+    {
+        AnnotationRegistry::registerAutoloadNamespace(
+            'EventStore\Common\Annotation',
+            __DIR__ . '/../../../../src'
+        );
+
+        $reader = new AnnotationReader();
+
+        $aggregateClass = new \ReflectionClass(Aggregate::class);
+
+        $aggregateConstructorMethod = $aggregateClass->getMethod('__construct');
+
+        $constructorAnnotation = $reader->getMethodAnnotation(
+            $aggregateConstructorMethod,
+            PublishDomainEvent::class
+        );
+
+        $this->assertInstanceOf(PublishDomainEvent::class, $constructorAnnotation);
+        $this->assertEquals('AggregateAdded', $constructorAnnotation->event());
+
+        $aggregateChangeNameMethod = $aggregateClass->getMethod('changeName');
+
+        $changeNameAnnotation = $reader->getMethodAnnotation(
+            $aggregateChangeNameMethod,
+            PublishDomainEvent::class
+        );
+
+        $this->assertInstanceOf(PublishDomainEvent::class, $changeNameAnnotation);
+        $this->assertEquals('AggregateNameChanged', $changeNameAnnotation->event());
+    }
+}
